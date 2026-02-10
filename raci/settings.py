ed6de5ecd5eb3d8b41ba57b6a364a14b19108e68
Antony <antomutwiri2001@gmail.com>
2026-02-10 12:28:32 +0300

    changed the templates

--- raci/settings.py
+++ raci/settings.py
@@ -269,15 +269,15 @@ WSGI_APPLICATION = "raci.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "raci_db.sqlite3",
-#         "OPTIONS": {          
-#             "timeout": 60,    
-#         },
-#     }
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "raci_db.sqlite3",
+        "OPTIONS": {          
+            "timeout": 60,    
+        },
+    }
+}
 
 # DATABASES = {
 #     'default': {
@@ -291,16 +291,16 @@ WSGI_APPLICATION = "raci.wsgi.application"
 # }
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv('DB_NAME'),
+#         'USER': os.getenv('DB_USER'),
+#         'PASSWORD': os.getenv('DB_PASSWORD'),
+#         'HOST': os.getenv('DB_HOST'),
+#         'PORT': os.getenv('DB_PORT'),
+#     }
+# }
 
 
 
