8f4b89b26d1eaa09dba615aaebba8a3caf38ec12
Antony <antomutwiri2001@gmail.com>
2026-02-20 16:20:27 +0300

    fix: settings databse setup

--- raci/settings.py
+++ raci/settings.py
@@ -290,6 +290,17 @@ WSGI_APPLICATION = "raci.wsgi.application"
 # }
 
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+    }
+}
+
 
 
 # Password validation
