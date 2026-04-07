37cc7aa22edd621f1073c60611325b55c7603885
Antony <antomutwiri2001@gmail.com>
2026-04-07 12:19:10 +0300

    fix: db  config changes

--- raci/settings.py
+++ raci/settings.py
@@ -271,15 +271,15 @@ FILE_UPLOAD_CHUNK_SIZE = 1024 * 1024
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "raci_db.sqlite3",
-        "OPTIONS": {          
-            "timeout": 60,    
-        },
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "raci_db.sqlite3",
+#         "OPTIONS": {          
+#             "timeout": 60,    
+#         },
+#     }
+# }
 
 # DATABASES = {
 #     'default': {
@@ -293,16 +293,16 @@ DATABASES = {
 # }
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.getenv('DB_NAME'),
-#         'USER': os.getenv('DB_USER'),
-#         'PASSWORD': os.getenv('DB_PASSWORD'),
-#         'HOST': os.getenv('DB_HOST'),
-#         'PORT': os.getenv('DB_PORT'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+    }
+}
 
 
 
